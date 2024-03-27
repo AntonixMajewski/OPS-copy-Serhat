c397dd7d3fc9d73b766665e51fb76856c9a557e6
serhatseval <serhatsevaal@gmail.com>
2024-03-27 20:18:38 +0100

    Essaaa

--- Lab - 6/Example Task/task_programs/client.c
+++ Lab - 6/Example Task/task_programs/client.c
@@ -9,6 +9,7 @@
 #include <time.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <signal.h>
 
 #define ERR(source) \
     (fprintf(stderr, "%s:%d\n", __FILE__, __LINE__), perror(source), kill(0, SIGKILL), exit(EXIT_FAILURE))
@@ -18,6 +19,7 @@ typedef struct queuenames
     char name_s[20];
     char name_d[20];
     char name_m[20];
+    pid_t name_c; 
 } queuenames;
 
 struct sum_message
@@ -46,22 +48,34 @@ void handler(int sig, siginfo_t *info, void *ucontext)
         exit(1);
     }
     printf("Received message: %s\n", msg);
-    
 }
 
-void ClientWork(queuenames q_names)
+void notification_function(int signo, siginfo_t *info, void *context)
 {
-    int pid = getpid();
-    char name_mq[20];
-    sprintf(name_mq, "/%d", pid);
-
-    mqd_t mq = mq_open(name_mq, O_RDWR | O_CREAT, 0666, NULL);
-    if (mq == -1)
+    printf("Notification received\n");
+    mqd_t mq = *(mqd_t *)(info->si_value.sival_ptr);
+    char msg[20]; // Adjust size as needed
+    if (mq_receive(mq, msg, 8192, NULL) == -1)
     {
-        perror("Queue creation failed");
+        perror("mq_receive");
         exit(1);
     }
-    printf("Queue created %s\n", name_mq);
+
+    struct sigevent sev;
+    sev.sigev_notify = SIGEV_SIGNAL; // Change to SIGEV_SIGNAL
+    sev.sigev_signo = SIGUSR1; // Specify the signal to be sent
+    sev.sigev_value.sival_ptr = &mq;
+    if (mq_notify(mq, &sev) == -1)
+    {
+        perror("mq_notify nf client");
+        exit(1);
+    }
+    printf("Received message: %s\n", msg);
+}
+
+void *write_to_queue(void *arg)
+{
+    queuenames q_names = *(queuenames *)arg;
 
     mqd_t mq_s = mq_open(q_names.name_s, O_RDWR);
     if (mq_s == -1)
@@ -69,39 +83,58 @@ void ClientWork(queuenames q_names)
         perror("Queue open failed");
         exit(1);
     }
-
-    sethandler(handler, SIGUSR1);
-
     while (1)
     {
         struct sum_message msg;
         printf("Enter two numbers (or 'q' to quit): ");
         fflush(stdout);
 
-        if (scanf("%d %d", &msg.a, &msg.b) != 2)
+        if (scanf("%d %d", &msg.a, &msg.b) == 2)
         {
-        }
+            char message[20];
+            sprintf(message,"%d %d %d", msg.a, msg.b, q_names.name_c);
+            printf("Sending message: %s\n", message);
 
-        char message[5];
-        sprintf(message, "%d %d", msg.a, msg.b);
+            if (mq_send(mq_s, message, strlen(message) + 1, 0) == -1)
+            {
+                perror("mq_send");
+            }
 
-        if (mq_send(mq_s, message, sizeof(message) + 1, 0) == -1)
-        {
-            perror("mq_send");
-            exit(1);
         }
-        struct sigevent sev;
-        sev.sigev_notify = SIGEV_SIGNAL;
-        sev.sigev_signo = SIGUSR1;
-        sev.sigev_notify_attributes = NULL;
-        sev.sigev_value.sival_ptr = &mq_s;
-        if (mq_notify(mq_s, &sev) == -1)
+        else
         {
-            perror("mq_notify");
-            exit(1);
+            break;
         }
     }
+    if (mq_close(mq_s) == -1)
+    {
+        perror("mq_close");
+        exit(1);
+    }
+
+    return NULL;
 }
+
+void *read_from_queue(void *arg)
+{
+    mqd_t mq = *(mqd_t *)arg;
+
+    struct sigevent sev;
+    sev.sigev_notify = SIGEV_SIGNAL;
+    sev.sigev_notify_attributes = NULL;
+    sev.sigev_signo = SIGUSR1;
+    sev.sigev_value.sival_ptr = &mq;
+    if (mq_notify(mq, &sev) == -1)
+    {
+        perror("mq_notify rq client");
+    }
+    while (1)
+    {
+        ;
+    }
+    return NULL;
+}
+
 
 int main(int argc, char **argv)
 {
@@ -114,6 +147,23 @@ int main(int argc, char **argv)
     sprintf(args.name_d, "/%s_d", argv[1]);
     sprintf(args.name_m, "/%s_m", argv[1]);
     printf("Queuesdfasas created %s %s %s\n", args.name_s, args.name_d, args.name_m);
-    ClientWork(args);
+
+    char pid_str[20];
+    sprintf(pid_str, "/%d", getpid());
+    args.name_c = getpid();
+
+    sethandler(notification_function, SIGUSR1);
+
+    mqd_t mq_mine = mq_open(pid_str, O_RDWR | O_CREAT, 0666, NULL);
+    pthread_t write_thread, read_thread;
+    pthread_create(&write_thread, NULL, write_to_queue, &args);
+
+
+    pthread_create(&read_thread, NULL, read_from_queue, &mq_mine);
+
+    pthread_join(write_thread, NULL);
+    pthread_join(read_thread, NULL);
+
+    // ClientWork(args);
     return 0;
 }
--- Lab - 6/Example Task/task_programs/server.c
+++ Lab - 6/Example Task/task_programs/server.c
@@ -29,26 +29,83 @@ void sethandler(void (*f)(int, siginfo_t *, void *), int sigNo)
         ERR("sigaction");
 }
 
+void notification_function(union sigval sv)
+{
+    mqd_t mq = *(mqd_t *)(sv.sival_ptr);
+    char message[20]; // Adjust size as needed
+    struct sigevent sev;
+    sev.sigev_notify = SIGEV_THREAD;
+    sev.sigev_notify_function = notification_function;
+    sev.sigev_notify_attributes = NULL;
+    sev.sigev_value.sival_ptr = sv.sival_ptr;
+    if (mq_notify(mq, &sev) == -1)
+    {
+        perror("mq_notify asjdnaklda");
+        exit(1);
+    }
+
+    if (mq_receive(mq, message, 8192, NULL) == -1)
+    {
+        perror("mq_receive");
+        exit(1);
+    }
+    struct sum_message msg;
+    pid_t pids = getpid();
+    sscanf(message, "%d %d %d", &msg.a, &msg.b, &pids);
+    char name_m[20];
+    sprintf(name_m, "/%d", pids);
+    printf("Received message: %s \n", name_m);
+    mqd_t mq_m = mq_open(name_m, O_RDWR, 0666, NULL);
+    int sum = msg.a + msg.b;
+    char *sum_str = (char *)malloc(20);
+    sprintf(sum_str, "%d", sum);
+    if (mq_send(mq_m, sum_str, sizeof(msg) + 1, 0) == -1)
+    {
+        perror("mq_send asdasd asda");
+        free(sum_str);
+        exit(1);
+    }
+    printf("Sent message: %s\n", sum_str);
+
+    // Re-register for the next message
+    
+}
 void sumHandler(int sig, siginfo_t *info, void *ucontext)
 {
     printf("Signal received\n");
     mqd_t mq = *(mqd_t *)info->si_value.sival_ptr;
     struct sum_message msg;
     char message[20];
-    if (mq_receive(mq, message, 8192, NULL) == -1)
-    {
-        perror("mq_receive");
-        exit(1);
-    }
+    static struct sigevent not ;
+    not .sigev_notify = SIGEV_THREAD;
+    not .sigev_notify_function = notification_function;
+    not .sigev_value.sival_ptr = info->si_value.sival_ptr;
+    if (mq_notify(mq, &not ) < 0)
+        ERR("mq_notify");
 
-    sscanf(message, "%d %d", &msg.a, &msg.b);
-    int sum = msg.a + msg.b;
-    char* sum_str = (char*)malloc(20);
-    sprintf(sum_str, "%d", sum);
-    if (mq_send(mq, sum_str, sizeof(msg), 0) == -1)
+    ssize_t received = mq_receive(mq, message, 8192, NULL);
+    if (received < 1)
     {
-        perror("mq_send");
-        exit(1);
+        if (errno != EAGAIN)
+        {
+            perror("mq_receive");
+            exit(1);
+        }
+    }
+    else
+    {
+        sscanf(message, "%d %d", &msg.a, &msg.b);
+        int sum = msg.a + msg.b;
+        char *sum_str = (char *)malloc(20);
+        sprintf(sum_str, "%d", sum);
+        if (mq_send(mq, sum_str, sizeof(msg), 0) == -1)
+        {
+            perror("mq_send");
+            free(sum_str); // Free the allocated memory before exiting
+            exit(1);
+        }
+        printf("Sent message: %s\n", sum_str);
+        free(sum_str); // Free the allocated memory after it's no longer needed
     }
 }
 
@@ -61,44 +118,38 @@ int main(int argc, char **argv)
     sprintf(name_m, "/%d_m", pid);
 
     mqd_t mq_s = mq_open(name_s, O_RDWR | O_CREAT, 0666, NULL);
-    mqd_t mq_d = mq_open(name_d, O_RDWR | O_CREAT, 0666, NULL);
-    mqd_t mq_m = mq_open(name_m, O_RDWR | O_CREAT, 0666, NULL);
-    if (mq_s == -1 || mq_d == -1 || mq_m == -1)
-    {
-        perror("Queue creation failed");
-        exit(1);
+    if (mq_s == (mqd_t) -1) {
+        perror("mq_open name_s");
+        exit(EXIT_FAILURE);
     }
+
+    mqd_t mq_d = mq_open(name_d, O_RDWR | O_CREAT, 0666, NULL);
+    if (mq_d == (mqd_t) -1) {
+        perror("mq_open name_d");
+        exit(EXIT_FAILURE);
+    }
+
+    mqd_t mq_m = mq_open(name_m, O_RDWR | O_CREAT, 0666, NULL);
+    if (mq_m == (mqd_t) -1) {
+        perror("mq_open name_m");
+        exit(EXIT_FAILURE);
+    }    
     printf("Queues created %s %s %s\n", name_s, name_d, name_m);
 
-    sethandler(sumHandler, SIGUSR1);
+
 
     struct sigevent sev;
-    sev.sigev_notify = SIGEV_SIGNAL;
-    sev.sigev_signo = SIGUSR1;
+    sev.sigev_notify = SIGEV_THREAD;
+    sev.sigev_notify_function = notification_function;
     sev.sigev_notify_attributes = NULL;
     sev.sigev_value.sival_ptr = &mq_s;
     if (mq_notify(mq_s, &sev) == -1)
     {
-        perror("mq_notify");
+        perror("mq_notify asdklamsdk");
         exit(1);
     }
 
-    pid_t child_pid = fork();
-    if (child_pid == -1)
-    {
-        perror("fork");
-        exit(1);
-    }
-    else if (child_pid == 0)
-    {
-        char pid_str[20];
-        sprintf(pid_str, "%d", pid);
-        printf("%s\n", pid_str);
-        execlp("./client", "./client", pid_str, NULL);
-        perror("execlp");
-        exit(1);
-    }
-
+    
     while (1)
     {
         ;
